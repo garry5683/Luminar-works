39f8bfee79e93f7e0801649e0d7f159b4d209e30
Garry David <garrydavid77492@gmail.com>
2021-06-13 22:47:43 +0530

    javascript

--- Javascript/operators.js
+++ Javascript/operators.js
@@ -4,7 +4,7 @@ console.log(10/3);
 console.log(10-2);
 console.log("10+2");//concatination
 
-var num1="10",num2="3",num3="3a",num4=3;
+var num1="10",num2="3",num3="3a",num4=10;
 
 console.log(num1+num2);
 console.log(num1-num2);
@@ -27,3 +27,38 @@ console.log(10!=20);
 console.log(10==20);
 console.log(true>false);
 console.log(true<false);          //false=0,true=1
+console.log(num1==num4);
+console.log(num1===num4);
+
+
+
+//logic operators
+
+
+
+
+// & => and 
+// | => or
+// ^ => xor
+
+
+
+//  x   y   x&y x|y x^y
+//  0   1    0   1   1
+//  1   1    1   1   0
+//  0   0    0   0   0
+//  1   0    0   1   1
+
+
+console.log(2&4);
+// 0010
+// 0100
+// 0000 => 0
+console.log(2|4);   
+// 0010
+// 0100
+// 0110 => 6
+console.log(2^4);
+// 0010
+// 0100
+// 0110 => 6
