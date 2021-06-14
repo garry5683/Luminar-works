ad8c4bfe0a129b13b2e69ecff0264b370032795d
Garry David <garrydavid77492@gmail.com>
2021-06-14 11:29:18 +0530

    javascript/flowcontrols/decisionmaking

--- Javascript/flowcontrols/decisionmaking/sortingofthreenumbers.js
+++ Javascript/flowcontrols/decisionmaking/sortingofthreenumbers.js
@@ -1,58 +1,58 @@
-var num1=15,num2=888,num3=110;
+//var num1=15,num2=888,num3=110;
 
-if(num1<=num2)
-{
-    if(num1<=num3)
-    {
-        console.log(`${num1}`);
-        if(num2<=num3)
-    {
-        console.log(`${num2}`); 
-        console.log(`${num3}`);
-    }
-    if(num2>=num3)
-    {
-        console.log(`${num3}`);
-        console.log(`${num2}`);
-    }
-}}
+//if(num1<=num2)
+//{
+//    if(num1<=num3)
+//    {
+//       console.log(`${num1}`);
+//        if(num2<=num3)
+//    {
+//        console.log(`${num2}`); 
+//       console.log(`${num3}`);
+//    }
+//    if(num2>=num3)
+//    {
+//        console.log(`${num3}`);
+//        console.log(`${num2}`);
+//    }
+//}}
     
     
 
-if(num2<=num1)
-{
-    if(num2<=num3)
-    {
-        console.log(`${num2}`);
-        if(num1<=num3)
-    {
-        console.log(`${num1}`); 
-        console.log(`${num3}`);
-    }
-    if(num1>=num3)
-    {
-        console.log(`${num3}`);
-        console.log(`${num1}`);
-    }
-    }
-    
-}
-
-if(num3<=num1)
-{
-    if(num3<=num2)
-    {
-        console.log(`${num3}`);
-        if(num1<=num2)
-    {
-        console.log(`${num1}`); 
-        console.log(`${num2}`);
-    }
-    if(num1>=num2)
-    {
-        console.log(`${num2},`);
-        console.log(`${num1}`);
-    }
-    }
-    
-}
+//if(num2<=num1)
+//{
+//    if(num2<=num3)
+//    {
+//        console.log(`${num2}`);
+//        if(num1<=num3)
+//    {
+//        console.log(`${num1}`); 
+//        console.log(`${num3}`);
+//    }
+//    if(num1>=num3)
+//    {
+//        console.log(`${num3}`);
+//        console.log(`${num1}`);
+//    }
+//    }
+//    
+//}
+//
+//if(num3<=num1)
+//{
+//    if(num3<=num2)
+//    {
+//        console.log(`${num3}`);
+//        if(num1<=num2)
+//    {
+//        console.log(`${num1}`); 
+//        console.log(`${num2}`);
+//    }
+//    if(num1>=num2)
+//    {
+//        console.log(`${num2},`);
+//        console.log(`${num1}`);
+//    }
+//    }
+//    
+//}
